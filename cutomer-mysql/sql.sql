5532c5c54ae9edc92befb433a4af63da64929c16
buglan <1831353087@qq.com>
2019-08-16 18:28:49 +0800

    feat: customer mysql docker image

---
+++ cutomer-mysql/sql.sql
@@ -0,0 +1,4 @@
+create database a default character set utf8mb4 collate utf8mb4_unicode_ci;
+create database b default character set utf8mb4 collate utf8mb4_unicode_ci;
+create database c default character set utf8mb4 collate utf8mb4_unicode_ci;
+create database c default character set utf8mb4 collate utf8mb4_unicode_ci;
