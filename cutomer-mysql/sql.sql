804fe60c104377362796f39b9c6ad0fbff58298e
buglan <1831353087@qq.com>
2019-08-18 12:57:01 +0800

    update customer mysql docker image

--- cutomer-mysql/sql.sql
+++ cutomer-mysql/sql.sql
@@ -1,3 +1,5 @@
+set global net_buffer_length=1000000;
+set global max_allowed_packet=1000000000;
 create database a default character set utf8mb4 collate utf8mb4_unicode_ci;
 create database b default character set utf8mb4 collate utf8mb4_unicode_ci;
 create database c default character set utf8mb4 collate utf8mb4_unicode_ci;
